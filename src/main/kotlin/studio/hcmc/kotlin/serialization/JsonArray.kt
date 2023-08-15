5f319aa30de5bba44d8c3e69df86589b84fcdf71
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-15 17:20:59 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/kotlin/serialization/JsonArray.kt
+++ src/main/kotlin/studio/hcmc/kotlin/serialization/JsonArray.kt
@@ -3,38 +3,46 @@ package studio.hcmc.kotlin.serialization
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.JsonArray
 
+@JvmName("toBooleanJsonArray")
 fun List<Boolean?>.toJsonArray(): JsonArray {
     return JsonArray(map(Boolean?::json))
 }
 
+@JvmName("toNumberJsonArray")
 fun List<Number?>.toJsonArray(): JsonArray {
     return JsonArray(map(Number?::json))
 }
 
+@JvmName("toUByteJsonArray")
 @ExperimentalSerializationApi
 fun List<UByte>.toJsonArray(): JsonArray {
     return JsonArray(map(UByte::json))
 }
 
+@JvmName("toUShortJsonArray")
 @ExperimentalSerializationApi
 fun List<UShort>.toJsonArray(): JsonArray {
     return JsonArray(map(UShort::json))
 }
 
+@JvmName("toUIntJsonArray")
 @ExperimentalSerializationApi
 fun List<UInt>.toJsonArray(): JsonArray {
     return JsonArray(map(UInt::json))
 }
 
+@JvmName("toULongJsonArray")
 @ExperimentalSerializationApi
 fun List<ULong>.toJsonArray(): JsonArray {
     return JsonArray(map(ULong::json))
 }
 
+@JvmName("toStringJsonArray")
 fun List<String?>.toJsonArray(): JsonArray {
     return JsonArray(map(String?::json))
 }
 
+@JvmName("toNothingJsonArray")
 @ExperimentalSerializationApi
 fun List<Nothing?>.toJsonArray(): JsonArray {
     return JsonArray(map(Nothing?::json))
